0dccd705d5d9a6d8510f51e45ff0dff491f27d6d
Alvar <alvararias@gmail.com>
2022-11-07 09:00:57 +0100

    Initial Commit

---
+++ testingAppXtest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testingAppXtest
+//
+//  Created by Alvar Arias on 2022-11-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testingAppXtest/testingAppXtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingAppXtestApp.swift
+//  testingAppXtest
+//
+//  Created by Alvar Arias on 2022-11-07.
+//
+
+import SwiftUI
+
+@main
+struct testingAppXtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
